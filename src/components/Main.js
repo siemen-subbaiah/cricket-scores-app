0e8e3c43981e989c3fa0f36febe298ff6d59e91f
siemen_subbaiah <62604902+siemen-subbaiah@users.noreply.github.com>
2023-07-31 10:16:06 +0000

    1. Testing codespaces

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import Matches from './Matches';
 
-const KEY = process.env.REACT_APP_API_KEY;
+// const KEY = process.env.REACT_APP_API_KEY;
+const KEY = 'c6cf75e3-60e9-45b8-901d-4db8919de80b';
 const Main = () => {
   const [matches, setMatches] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://cricapi.com/api/matches?apikey=${KEY}`)
+    fetch(`https://api.cricapi.com/v1/matches?apikey=${KEY}`)
       .then((res) => res.json())
       .then((data) => {
-        setMatches(data.matches);
+        setMatches(data.data);
         setLoading(false);
       });
   }, []);
 
   return (
     <main>
-      <h1 className="upcoming">UPCOMING MATCHES</h1>
+      <h1 className='upcoming'>UPCOMING MATCHES</h1>
       {loading ? (
-        <h1 className="loding">LOADING...</h1>
+        <h1 className='loding'>LOADING...</h1>
       ) : (
-        <div className="matches">
+        <div className='matches'>
           {matches.map((match) => (
             <Matches match={match} key={match.unique_id} />
           ))}
--- src/components/Matches.js
+++ src/components/Matches.js
@@ -4,9 +4,9 @@ const Matches = ({ match }) => {
   const [score, setScore] = useState({});
   const [loading, setLoading] = useState(true);
   const [toggle, setToggle] = useState(false);
-  let matchType = match.type === 'Twenty20' ? 'T20' : 'N/A';
 
-  const KEY = process.env.REACT_APP_API_KEY;
+  // const KEY = process.env.REACT_APP_API_KEY;
+  const KEY = "mirocey224@sportrid.com"
   const getScore = () => {
     fetch(
       `https://cricapi.com/api/cricketScore?apikey=${KEY}&unique_id=${match.unique_id}`
@@ -28,15 +28,11 @@ const Matches = ({ match }) => {
       <div className="match">
         <div className="match-main-info">
           <div className="main-info">
-            <h2>{match['team-1']}</h2>
-          </div>
-          <div className="main-info2">
-            <h2>{match[`team-2`]}</h2>
+            <h2>{match.name}</h2>
           </div>
         </div>
         <div className="match-sub-info">
-          <h2 className="match-type">{matchType}</h2>
-          <p>TOSS : {match.toss_winner_team || 'N/A'}</p>
+          <h2 className="match-type">{match.matchType}</h2>
           <p> {new Date(match.date).toLocaleDateString()}</p>
           <button className="btn" onClick={handleScore}>
             SCORE
